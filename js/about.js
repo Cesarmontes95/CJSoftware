996436a56bab044b255091dc0f123b7995e4ab7a
Cesar Montes <92511593+Cesarmontes95@users.noreply.github.com>
2022-10-21 00:39:34 -0600

    Update about.js

--- js/about.js
+++ js/about.js
@@ -103,7 +103,7 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 var typed = new Typed(".typing",{
-    strings: ["Cesar Montes", "CJ Software", "Johann Roman"],
+    strings: ["César", "Catalyze", "Johann", "Jorge","Johnny"],
     typeSpeed: 120,
     backSpeed: 60,
     loop: true
