db440de481f27218528a9e30fbe6a5b672a2c338
Cesar Montes <92511593+Cesarmontes95@users.noreply.github.com>
2022-10-21 00:17:21 -0600

    Update inicio.js

--- js/inicio.js
+++ js/inicio.js
@@ -15,7 +15,7 @@ showMenu('nav-toggle','nav-menu')
 /*TYPING*/
 
 var typed = new Typed(".typing", {
-    strings: ["Cesar Montes", "CJ Software", "Johann Román"],
+    strings: ["César", "Catalyze", "Johann", "Jorge", "Johnny"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
@@ -23,21 +23,21 @@ var typed = new Typed(".typing", {
 
 /*----- ANIMATE -----*/
 // OVERLAY
-gsap.to(".first", 1.5, {delay: .5, top: "-100%", ease: Expo.easeInOut});
-gsap.to(".second", 1.5, {delay: .7, top: "-100%", ease: Expo.easeInOut});
-gsap.to(".third", 1.5, {delay: .9, top: "-100%", ease: Expo.easeInOut});
+//gsap.to(".first", 1.5, {delay: .5, top: "-100%", ease: Expo.easeInOut});
+//gsap.to(".second", 1.5, {delay: .7, top: "-100%", ease: Expo.easeInOut});
+//gsap.to(".third", 1.5, {delay: .9, top: "-100%", ease: Expo.easeInOut});
 
 // IMG
-gsap.from('.home__img', {opacity: 0, duration: 2, delay: 2, x: 60})
+gsap.from('.home__img', {opacity: 0, duration: 1, delay: 1, x: 60})
 
 // INFORMATION
-gsap.from('.home__information', {opacity: 0, duration: 3, delay: 2.3, y: 25})
-gsap.from('.anime-text', {opacity: 0, duration: 3, delay: 2.3, y: 25, ease:'expo.out', stagger: .3})
+gsap.from('.home__information', {opacity: 0, duration: 2, delay: 1.5, y: 25})
+gsap.from('.anime-text', {opacity: 0, duration: 2, delay: 1.5, y: 25, ease:'expo.out', stagger: .3})
 
 // NAV ITEM
-gsap.from('.nav__logo', {opacity:0, duration: 3, delay: 3.2, y: 25, ease:'expo.out'});
-gsap.from('.nav__item', {opacity: 0, duration: 3, delay: 3.2, y: 25, ease:'expo.out', stagger: .2})
+gsap.from('.nav__logo', {opacity:0, duration: 2, delay: 2, y: 25, ease:'expo.out'});
+gsap.from('.nav__item', {opacity: 0, duration: 2, delay: 2, y: 25, ease:'expo.out', stagger: .2})
 
 // SOCIAL
-gsap.from('.home__social-icon', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})
-gsap.from('.home__social-insta', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})
+gsap.from('.home__social-icon', {opacity: 0, duration: 2, delay: 2, y: 25, ease:'expo.out', stagger: .2})
+gsap.from('.home__social-insta', {opacity: 0, duration: 2, delay: 2, y: 25, ease:'expo.out', stagger: .2})
